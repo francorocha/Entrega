c3e065632358de56f539ff8f21360c8642b998cd
francorocha <fraivaroccar@gmail.com>
2020-08-13 19:12:50 -0300

    mi primer commit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,19 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let users = Array({
+        usuario: email.value,
+        contraseña: pass.value
+    });
+    localStorage.setItem('users',JSON.stringify(users));
+    location.href='index.html';
+});
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
